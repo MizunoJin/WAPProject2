7a3cafbf1a19eb36f5e65ce920643af61c53af07
Jin Mizuno <bump.lk_k.1215@icloud.com>
2024-04-15 21:19:29 +0100

    Add authentication and authorization function

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,20 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { useNavigate } from 'react-router-dom';
 
 function Header() {
+  const navigate = useNavigate();
+
+  const handleSignOut = () => {
+    localStorage.removeItem('accessToken');
+    navigate('/login');
+  };
+
+  const isAuthenticated = () => {
+    return !!localStorage.getItem('accessToken');
+  };
+
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
@@ -10,9 +22,18 @@ function Header() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/profile">Profile</Nav.Link>
-            <Nav.Link href="#signout">Sign Out</Nav.Link>
+            {isAuthenticated() ? (
+              <>
+                <Nav.Link href="/">Home</Nav.Link>
+                <Nav.Link href="/profile">Profile</Nav.Link>
+                <Nav.Link onClick={handleSignOut} href="#">Sign Out</Nav.Link>
+              </>
+            ) : (
+              <>
+                <Nav.Link href="/login">Login</Nav.Link>
+                <Nav.Link href="/signup">Signup</Nav.Link>
+              </>
+            )}
           </Nav>
         </Navbar.Collapse>
       </Container>
